ee17cd5bb4959fd95855e2c73666c97e843df6ba
Sergey Shelomentsev <sergey.v.shelomentsev@gmail.com>
2019-10-02 15:16:06 +0300

    Add initial scheduler code

--- src/main/java/com/sshelomentsev/scheduler/Scheduler.java
+++ src/main/java/com/sshelomentsev/scheduler/Scheduler.java
@@ -1,4 +1,64 @@
 package com.sshelomentsev.scheduler;
 
-public class Scheduler {
+import java.util.Date;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class Scheduler extends TimerTask {
+
+    private ConcurrentHashMap<Long, CopyOnWriteArrayList<Task>> map = new ConcurrentHashMap<>();
+    private volatile boolean isRunning = false;
+    private AtomicLong timestamp = new AtomicLong(System.currentTimeMillis());
+    private Timer[] timers;
+    private List<Task> currentQueue;
+    private final int threadsNumber;
+
+    public Scheduler(final int threadsNumber) {
+        this.threadsNumber = threadsNumber;
+        this.timers = new Timer[threadsNumber];
+        for (int i = 0; i < threadsNumber; i++) {
+            this.timers[i] = new Timer();
+        }
+    }
+
+    public Scheduler() {
+        this(1);
+    }
+
+    public void addTask(Task task) {
+        map.computeIfAbsent(task.getTimestamp(), s -> new CopyOnWriteArrayList<>()).add(task);
+    }
+
+    @Override
+    public void run() {
+        if (!isRunning) {
+            isRunning = true;
+            if (currentQueue.size() > 0) {
+                Date date = new Date(timestamp.get());
+                for (int i = 0; i < threadsNumber; i++) {
+                    if (currentQueue.size() > 0) {
+                        Task task = currentQueue.remove(0);
+                        System.out.println("schedule " + task.toString() + "at " + System.currentTimeMillis());
+                        timers[i].schedule(new TimerTask() {
+                            @Override
+                            public void run() {
+                                task.run();
+                            }
+                        }, date);
+                    }
+                }
+            }
+            if (currentQueue.size() < threadsNumber) {
+                long ts = timestamp.incrementAndGet();
+                currentQueue.addAll(map.getOrDefault(ts, new CopyOnWriteArrayList<>()));
+                map.remove(ts);
+            }
+
+            isRunning = false;
+        }
+    }
 }
--- src/main/java/com/sshelomentsev/scheduler/Task.java
+++ src/main/java/com/sshelomentsev/scheduler/Task.java
@@ -1,4 +1,30 @@
 package com.sshelomentsev.scheduler;
 
-public class Task {
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.TimerTask;
+import java.util.concurrent.Callable;
+
+public class Task extends TimerTask {
+
+    private final LocalDateTime time;
+    private final Callable callable;
+
+    public Task(LocalDateTime time, Callable callable) {
+        this.time = time;
+        this.callable = callable;
+    }
+
+    public long getTimestamp() {
+        return time.toInstant(ZoneOffset.UTC).toEpochMilli();
+    }
+
+    @Override
+    public void run() {
+        try {
+            callable.call();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
